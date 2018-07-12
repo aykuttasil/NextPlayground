712b4e3fbadbc57d5aee830d20784bc5ae325b89
Aykut Asil <aykuttasil@gmail.com>
2018-07-13 02:39:51 +0300

    client side route

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+// This is the Link API
+import Link from 'next/link'
+
 const Index = () => (
     <div>
+        <Link href="/about">
+            <a>About Page</a>
+        </Link>
+        <br/>
+        <Link href="/about">
+            <a style={{
+                fontSize: 30
+            }}>About Page</a>
+        </Link>
+        <br/>
+
+        <Link href="/about">
+            <button>Go to About Page</button>
+        </Link>
+
         <p>Hello Next.js</p>
     </div>
 )
